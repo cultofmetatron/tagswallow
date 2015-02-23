d75b9d6a3807445fbb1b8565466d924d8a4373cc
Peter de Croos <cultofmetatron@aumlogic.com>
2015-02-23 13:44:29 -0800

    added some stuff

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -13,8 +13,33 @@ console.log('Booting up our components!!');
 var React = require('react');
 
 var Navbar = require('./components/navbar');
+var Rx = require('Rx');
 
 React.render(<Navbar></Navbar>, document.body);
+
+var mouseMove = Rx.Observable.fromEvent(document, 'mousemove');
+
+mouseMove
+  .map(function(ev) {
+    return {
+      x: ev.clientX,
+      y: ev.clientY
+    };
+  })
+  .pairwise(2)
+  .map(function(polls) {
+    var pos1 = polls[0];
+    var pos2 = polls[1];
+    return {
+      x: pos2.x - pos1.x,
+      y: pos2.y - pos1.y
+    }
+  })
+  .subscribe(function(ev) {
+    console.log(ev.x, ev.y)
+  }, function(err) {
+    console.log(err);
+  });
 
 
 
